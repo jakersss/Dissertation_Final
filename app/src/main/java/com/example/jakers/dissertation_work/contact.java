7ba4d80b39bcdfde8cb2e503b2fc1412d9c7c8cb
jaketheflake <jakehedges123@hotmail.com>
2017-11-17 10:05:08 +0000

    got page changes working

--- app/src/main/java/com/example/jakers/dissertation_work/contact.java
+++ app/src/main/java/com/example/jakers/dissertation_work/contact.java
@@ -1,10 +1,16 @@
 package com.example.jakers.dissertation_work;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 /**
  * Created by Jakers on 17/11/2017.
  */
 
 public class contact extends AppCompatActivity {
-}
+    public class calendar extends AppCompatActivity {
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.contact_layout);
+        }}}
