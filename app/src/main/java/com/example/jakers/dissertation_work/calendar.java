9a1f0e754fde4e058aaef5903cfefa2e02cc433a
jaketheflake <jakehedges123@hotmail.com>
2017-11-17 19:24:52 +0000

    got calendar functionality working, not yet optimised

--- app/src/main/java/com/example/jakers/dissertation_work/calendar.java
+++ app/src/main/java/com/example/jakers/dissertation_work/calendar.java
@@ -7,10 +7,11 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
-import java.time.*;
 import java.util.Locale;
 import java.util.Calendar;
 import java.util.Date;
+
+import io.blackbox_vision.materialcalendarview.view.CalendarView;
 
 /**
  * Created by Jakers on 17/11/2017.
@@ -22,6 +23,22 @@ public class calendar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calendar_layout);
+
+        CalendarView calendarView = (CalendarView) findViewById(R.id.calendar_view);
+
+        calendarView.shouldAnimateOnEnter(true);
+                //.setFirstDayOfWeek(Calendar.MONDAY)
+                //.setOnDateClickListener(this::onDateClick)
+                //.setOnMonthChangeListener(this::onMonthChange)
+                //.setOnDateLongClickListener(this::onDateLongClick)
+                //.setOnMonthTitleClickListener(this::onMonthTitleClick);
+
+        //if(calendarView.isMultiSelectDayEnabled()) {
+        //    calendarView.setOnMultipleDaySelectedListener(this::onMultipleDaySelected);
+        //}
+
+        calendarView.update(Calendar.getInstance(Locale.getDefault()));
+
 
         //getActionBar().setTitle("hi there");
         //getSupportActionBar().setTitle("hi there");
