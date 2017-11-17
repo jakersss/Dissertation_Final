dd98b50d9829f4d48e431fc9a21cf0f44b982bd5
jaketheflake <jakehedges123@hotmail.com>
2017-11-17 12:30:01 +0000

    added titles to all pages, except calendar

--- app/src/main/java/com/example/jakers/dissertation_work/calendar.java
+++ app/src/main/java/com/example/jakers/dissertation_work/calendar.java
@@ -1,9 +1,16 @@
 package com.example.jakers.dissertation_work;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+
+import java.time.*;
+import java.util.Locale;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by Jakers on 17/11/2017.
@@ -15,6 +22,8 @@ public class calendar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calendar_layout);
-        getActionBar().setTitle("hi there");
-        getSupportActionBar().setTitle("hi there");
-}}
+
+        //getActionBar().setTitle("hi there");
+        //getSupportActionBar().setTitle("hi there");
+    }
+}
