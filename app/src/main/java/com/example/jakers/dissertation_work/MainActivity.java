c0dcf5580c1e4308aec4bc533a1a76e2cd28d00c
jaketheflake <jakehedges123@hotmail.com>
2017-11-17 09:47:55 +0000

    menu functionality added, working fine.

--- app/src/main/java/com/example/jakers/dissertation_work/MainActivity.java
+++ app/src/main/java/com/example/jakers/dissertation_work/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.jakers.dissertation_work;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -80,20 +81,22 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.nav_calendar) {
+            Intent intent = new Intent(this, calendar.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_contact) {
+            Intent intent = new Intent(this, contact.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_homework) {
+            Intent intent = new Intent(this, homework.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_map) {
+            Intent intent = new Intent(this, map.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_subject){
+            Intent intent = new Intent(this, subject.class);
+            startActivity(intent);
         }
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
---
+++ app/src/main/java/com/example/jakers/dissertation_work/subject.java
@@ -0,0 +1,10 @@
+package com.example.jakers.dissertation_work;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Jakers on 17/11/2017.
+ */
+
+public class subject extends AppCompatActivity  {
+}
