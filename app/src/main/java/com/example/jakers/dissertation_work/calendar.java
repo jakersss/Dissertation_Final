00e8ae31926d573a4f9146d2f5f631ed875c8203
jaketheflake <jakehedges123@hotmail.com>
2017-11-17 11:52:08 +0000

    Added name changes to pages

--- app/src/main/java/com/example/jakers/dissertation_work/calendar.java
+++ app/src/main/java/com/example/jakers/dissertation_work/calendar.java
@@ -1,6 +1,7 @@
 package com.example.jakers.dissertation_work;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -8,8 +9,12 @@ import android.support.v7.widget.Toolbar;
  * Created by Jakers on 17/11/2017.
  */
 
+// Add string to index
+
 public class calendar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calendar_layout);
+        getActionBar().setTitle("hi there");
+        getSupportActionBar().setTitle("hi there");
 }}
